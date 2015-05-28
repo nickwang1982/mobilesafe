ad73dcac4723aa3065fe26adff6a3827883fefcd
nick wang <nickwang19820329@gmail.com>
2015-05-28 21:47:29 +0800

    Revert project to nothing

--- app/src/main/java/com/example/course/mobilesafe/SplashActivity.java
+++ app/src/main/java/com/example/course/mobilesafe/SplashActivity.java
@@ -16,39 +16,7 @@ public class SplashActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Set as no title window
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
-        tv_splash_version = (TextView)findViewById(R.id.tv_splash_version);
-        tv_splash_version.setText(getString(R.string.label_version) + getVersion());
     }
 
-
-    /**
-     * Get current app version
-     *
-     * @return version
-     */
-    private String getVersion() {
-
-        String currentVersion = null;
-        // Use system package manager
-        PackageManager pm = this.getPackageManager();
-
-        try {
-            // Param1: current application name
-            // Param2: additional
-            PackageInfo info = pm.getPackageInfo(getPackageName(), 0);
-            // return current version
-            currentVersion = info.versionName;
-
-        } catch (Exception e) {
-            // No package name found exception
-            e.printStackTrace();
-            return null;
-        }
-        return currentVersion;
-    }
 }
